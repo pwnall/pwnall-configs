16541b54b2cb897a4c8a226a89cc4ba670c34dab
Victor Costan <costan@gmail.com>
2025-07-04 22:47:45 -0700

    Bash, SSH, VS Code.

---
+++ vscode-extensions.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e  # Exit on errors.
+set -u  # Exit on undefined variables.
+
+# Proven
+code --install-extension bazelbuild.vscode-bazel
+code --install-extension bierner.markdown-mermaid
+code --install-extension fuchsia-authors.language-fidl
+code --install-extension fuchsia-authors.vscode-fuchsia
+code --install-extension drblury.protobuf-vsc
+code --install-extension golang.go
+code --install-extension google.geminicodeassist
+code --install-extension google.gemini-cli-vscode-ide-companion
+code --install-extension google.gn
+code --install-extension leanprover.lean4
+code --install-extension llvm-vs-code-extensions.vscode-clangd
+code --install-extension mathematic.vscode-pdf
+code --install-extension mrmlnc.vscode-json5
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-python.vscode-python-envs
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code --install-extension rust-lang.rust-analyzer
+code --install-extension tintinweb.graphviz-interactive-preview
+code --install-extension vadimcn.vscode-lldb
+code --install-extension vscodevim.vim
+code --install-extension webfreak.debug
+
+# Trying out
+# Cline: code --install-extension saoudrizwan.claude-dev
